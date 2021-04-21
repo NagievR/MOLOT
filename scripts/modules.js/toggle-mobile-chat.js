de4b73e98b5a0eaa8c15c93868acc278f9f3a40b
Ruslan Nagiev <ultraduck100@gmail.com>
2021-04-21 15:57:39 +0300

    add basic mobile adapt

--- scripts/modules.js/toggle-mobile-chat.js
+++ scripts/modules.js/toggle-mobile-chat.js
@@ -1,12 +1,15 @@
 const recent = document.querySelector('.recent');
 const chat = document.querySelector('.chat');
+const bottomBlock = document.querySelector('.bottom-block');
 
+const container = document.querySelector('.chat-page__container');
 // recent.style.display = 'none';
 // chat.style.display = 'block';
 
 export const toggleMobileChat = () => {
   recent.addEventListener('click', () => {
-    console.log('ss');
+    container.style.display = 'none';
+    bottomBlock.style.display = 'none';
 
     recent.style.display = 'none';
     chat.style.display = 'block';
