64cdfdb1a6aeb9cd7797963a56432de20696dfb0
nagievr <ruslannagiev96@gmail.com>
2021-04-23 02:34:21 +0300

    fix some bugs

--- scripts/modules.js/toggle-mobile-chat.js
+++ scripts/modules.js/toggle-mobile-chat.js
@@ -2,13 +2,13 @@ const recent = document.querySelector('.recent');
 const chat = document.querySelector('.chat');
 const bottomBlock = document.querySelector('.bottom-block');
 
-const container = document.querySelector('.chat-page__container');
+const blockToHide = document.querySelector('.chat-page__header');
 // recent.style.display = 'none';
 // chat.style.display = 'block';
 
 export const toggleMobileChat = () => {
   recent.addEventListener('click', () => {
-    container.style.display = 'none';
+    blockToHide.style.display = 'none';
     bottomBlock.style.display = 'none';
 
     recent.style.display = 'none';
